68ca80acb7c10bec10dd8c3b9d3b6786bb4e5441
SeanM187 <125615741+SeanM187@users.noreply.github.com>
2023-05-26 11:50:55 +1200

    Add files via upload

---
+++ 02_num_checker.py
@@ -0,0 +1,25 @@
+# checks that input is either float or an
+# integer that is more that zero. Takes in custom error message
+def num_check(question, error, num_type):
+    valid = False
+
+    while not valid:
+
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print()
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# Main routine goes here
+get_int = num_check("How many do you need? ", "Please enter an amount more than 0\n", int)
+get_cost = num_check("How much does it cost?", "Please enter a number more than 0\n", float)
+
+print("You need: {}".format(get_int))
+print("It costs: ${}".format(get_cost))
---
+++ 03_get_ingredients.py
@@ -0,0 +1,82 @@
+import pandas as pd
+
+
+# function to check if the input is 'yes' or 'no'
+def yes_no(question):
+    to_check = ["yes", "no"]
+    while True:
+        response = input(question).lower()
+        if response in to_check:
+            return response
+        else:
+            print("Please enter either 'yes' or 'no'.\n")
+
+
+# function to check if the input is a positive number
+def num_check(question, error, num_type):
+    while True:
+        try:
+            response = num_type(input(question))
+            if response <= 0:
+                print(error)
+            else:
+                return response
+        except ValueError:
+            print(error)
+
+
+# function to check if the input is not blank
+def not_blank(question, error):
+    while True:
+        response = input(question)
+        if response == "":
+            print(error)
+        else:
+            return response
+
+
+# get recipe name and number of servings
+recipe_name = not_blank("Enter your Recipe Name: ", "Recipe Name can't be blank")
+per_serve = num_check("How many servings? ", "Please enter a number greater than 0.", int)
+
+# get amount, unit, and ingredient
+ingredients = []
+amounts = []
+units = []
+
+ingredients_dict = {
+    "Amount": amounts,
+    "Unit": units,
+    "Ingredient": ingredients
+}
+
+print("\nEnter the details for each ingredient (enter 'xxx' to finish):\n")
+
+while True:
+    ingredient = not_blank("Ingredient Name: ",
+                           "The item name can't be blank.")
+    if ingredient.lower() == "xxx":
+        # check if user entered an ingredient
+        if len(ingredients) == 0:
+            print("You cannot enter 'xxx' without entering and ingredients")
+            ingredients = ""
+            continue
+        else:
+            break
+
+    amount = num_check("Amount (in grams, milliliters, etc.): ", "Please enter a number greater than 0.", float)
+    unit = not_blank("Unit of measurement: ", "The unit of measurement can't be blank.")
+
+    ingredients.append(ingredient)
+    amounts.append(amount)
+    units.append(unit)
+
+# Create a DataFrame to display the ingredient details
+
+df = pd.DataFrame(ingredients_dict)
+
+# printing area
+print(f'-----{recipe_name}-----')
+print(f'Servings: {per_serve}')
+print("\n======Recipe Ingredients======")
+print(df.to_string(index=False))
